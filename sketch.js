f428556897861a1757d3808f918107df50e0c4ce
jmsaavedra <saavedra@jos.ph>
2016-09-14 11:38:08 -0400

    updated site

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
 function draw() {
+  background('rgba(0,0,0, 0.1)');
   textSize(32);
-  fill(mouseX/200, mouseY/200);
   text("hello github.io!", mouseX, mouseY);
+}
+
+function mouseMoved(){
+  fill(random(255), random(255), random(255));
 }
